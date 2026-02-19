9f5db1242237b807988ca76d92e16326e8a5cdba
Thorat Soham <djsoham02@gmail.com>
2026-02-19 07:41:44 +0530

    Reverted Auth.js

--- js/auth.js
+++ js/auth.js
@@ -247,7 +247,7 @@ const auth = {
             btn.disabled = true;
             btn.textContent = 'Sending...';
 
-            const siteUrl = https://thoratsoham.github.io/Noire-Fashion/;
+            const siteUrl = window.location.origin + window.location.pathname;
 
             const { error } = await window.supabaseClient.auth.signInWithOtp({
                 email,
